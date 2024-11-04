b9ef1ba8a90fb0a8ddbad34019fd18f907c68bcd
paloma208 <136338752+paloma208@users.noreply.github.com>
2024-11-04 11:42:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Automotivo',
+    'Qual a diferença entre V8 e 8v?',
+    'V8 refere-se a um motor com oito cilindros desejado em "V", enquanto 8v indica um motor com oito válvulas, normalmente em quatro cilindros. Sendo assim o motor V8 é mais potente que o motor 8v.'
+
+)
+
+criaCartao(
+    'Automotivo',
+    'Quais são as características que definem um carro esportivo?',
+    'Desempenho, Manuseio, Peso, Design aerodinâmico, Freios de alto desempenho, Conforto esportivo, Tração e sonoridade.'
+)
+
+criaCartao(
+    'Automotivo',
+    'Qual modificação considera um carro a ser esportivo?',
+    'Aprimoramento, Suspensão esportiva, Freios de alto desempenho, Redução de peso, modificação aerodinâmica e Rodas e Pneus.'
+)
+
+criaCartao(
+    'Automotivo',
+    'Quais marcas são reconhecidas por seus carros esportivos icônicos?',
+    'Ferrari, Porshe, Lamborghini, Maserati, Chevrolet, Ford, Nissan, McLaren, Astron Martin, Toyota.'
+
+)
